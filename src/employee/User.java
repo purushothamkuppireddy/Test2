b6f6a3416d6f8cace156646b3f4cd4482695165f
Kuppireddy Purushotham Reddy <purushothamreddy9085@gmail.com>
2019-03-02 11:51:25 +0530

    assesment

--- src/employee/User.java
+++ src/employee/User.java
@@ -3,49 +3,27 @@ package employee;
 import java.util.ArrayList;
 import java.util.Scanner;
 
- class Details {
-
-	
-	Scanner sc=new Scanner(System.in);	
+ 
 	
 	
-	private String name;
-	 public Scanner getSc() {
-		return sc;
-	}
-	public void setSc(Scanner sc) {
-		this.sc = sc;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getPhone() {
-		return phone;
-	}
-	public void setPhone(long phone) {
-		this.phone = phone;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	private long phone;
-	 private int age;
-	
-		
-	}
-
 public class User{
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		
-		
-		
+		Scanner sc=new Scanner(System.in);	
+	System.out.println("enetr the user details");	
+	 String name= sc.next();
+	 long phone=sc.nextLong();
+	 int age;
+int ag=sc.nextInt();
+if(ag<20||ag>55)
+	throw new Exception("age is not sufficient"); 
+else
+	age=ag;
+	
+
+System.out.println(name);
+System.out.println(phone);	
+System.out.println(age);
 	}
 	
 	
